42aa19782afe908a91c915e90125681b8159d842
Nahl <nahlultra@gmail.com>
2021-04-02 16:52:51 +0700

    init

---
+++ android/app/src/main/kotlin/com/example/maimunah_binti_alharist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.maimunah_binti_alharist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/home.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: DefaultTextStyle(
+          style: GoogleFonts.workSans(color: Colors.black),
+          child: ListView(
+            padding: EdgeInsets.only(right: 24, left: 24, top: 64, bottom: 0),
+            children: [
+              Text("Maimunah binti Al-Harist",
+                  style: GoogleFonts.workSans(
+                      fontWeight: FontWeight.bold, fontSize: 42)),
+              Text("Biografi", style: TextStyle(fontSize: 24)),
+              SizedBox(height: 64),
+              Text(
+                  '''Ummul Mukminin Maimunah binti al-Harits bin Hazn al-Hilali adalah wanita terakhir yang dinikahi oleh Nabi shalallallahu ‘alaihi wa sallam. Tidak banyak yang mengenal wanita bangsawan Bani Hilal ini. Sekarang, mari kita mengenalnya. Karena ia adalah ibu kita, ibu orang-orang yang beriman.
+
+Nasabnya
+
+Ummul Mukminin Maimunah merupakan putri dari al-Harits bin Hazn bin Bujair bin al-Hazm bin Ruwaibah bin Abdullah bin Hilal. Beliau lahir 29 tahun sebelum hijrah dan wafat tahun 51 H. Bersamaan dengan 593 M dan wafat tahun 671 M. Ibunya bernama Hind binti Auf bin Zuhair bin al-Harits bin Hamathah bin Humair.
+
+Saudara kandung Ummul Mukminin Maimunah adalah Ummul Fadhl Lubabah al-Kubra binti al-Harits yang merupakan istri dari al-Abbas bin Abdul Muthalib, paman Nabi Muhammad shallallahu ‘alaihi wa sallam. Kemudian Lubabah ash-Shugra Ashma binti al-Harits. Ia adalah istri dari al-Walid bin al-Mughirah dan ibu dari Khalid bin al-Walid Saifullah radhiallahu ‘anhu. Kemudian Lubabah ash-Shugra menikah dengan Ubay bin Khalaf al-Jahmi. Darinya, ia memiliki anak yang bernama Aban dan selainnya. Saudari berikutnya adalah Izzah binti al-Harits yang merupakan istri dari Ziyad bin Abdullah bin Malik. Mereka semua merupakan saudara se-ayah dan se-ibu Maimunah. Adapun saudarinya se-ibu saja adalah Asma binti Umais yang merupakan istri dari Ja’far bin Abu Thalib (Muhibbuddin ath-Thabari: as-Simthu ats-Tsamin, Hal: 189)
+
+Ada yang menyatakan, wanita yang memiliki menantu-menantu terbaik di dunia ini adalah Hind binti Auf bin al-Harits, ibu dari Ummul Mukminin Maimunah radhiallahu ‘anha. Menantunya adalah al-Abbas dan Hamzah. Dua orang putra Abdul Muthalib. Abbas menikah dengan putrinya Lubabah al-Kubra binti al-Harits. Dan Hamzah menikahi Salma binti Umais.
+
+Mantu lainnya adalah Ja’far dan Ali. Dua orang putra Abu Thalib. Keduanya menikah dengan Asma binti Umais. Pertama Asma dinikahi Ja’far. Setelah Ja’far gugur di Perang Mu’tah dan Asma selesai masa iddahnya, ia dinikahi oleh Abu Bakar ash-Shiddiq. Kemudian baru dinikahi Ali bin Abu Thalib. Saat Hamzah wafat di Perang Uhud, Salma dinikahi oleh Usamah bin al-Hadi al-Laitsi.
+
+Dan menantu terbaiknya tentu saja Rasulullah shallallahu ‘alaihi wa sallam yang menikahi dua orang putrinya. Pertama Ummul Mukminin Zainab binti Khuzaimah radhiallahu ‘anha. Setelah ia wafat, di masa berikutnya Rasulullah menikahi Ummul Mukminin Maimunah binti al-Harits (al-Ashami: Simthu an-Nujum al-Awali, 1/201).
+
+Kalau hanya melihat dari sudut duniawi; kedudukan, nasab, dan ketokohan, tentu nama al-Walid bin al-Mughirah -ayah Khalid bin al-Walid- akan menjadi pelengkap betapa menterengnya menantu-menantu Hind binti Auf. Namun sudut pandang ukhrawi lebih kita kedepankan. Sehingga tak menjadikan al-Walid sebagai tambahan kemuliaan untuknya. Allah Ta’ala berifrman tentang nilai seorang yang kafir,
+
+أُولَٰئِكَ الَّذِينَ كَفَرُوا بِآيَاتِ رَبِّهِمْ وَلِقَائِهِ فَحَبِطَتْ أَعْمَالُهُمْ فَلَا نُقِيمُ لَهُمْ يَوْمَ الْقِيَامَةِ وَزْنًا
+
+“Mereka itu orang-orang yang telah kufur terhadap ayat-ayat Tuhan mereka dan (kufur terhadap) perjumpaan dengan Dia, maka hapuslah amalan-amalan mereka, dan Kami tidak mengadakan suatu penilaian bagi mereka pada hari kiamat.” [Quran Al-Kahfi: 105].
+
+Kemuliaannya
+
+Ummul Mukminin Maimunah binti al-Harits dihormati oleh istri-istri yang lain. Karena ia adalah saudari dari Ummul Fadhl, istri paman nabi, al-Abbas bin Abdul Muthalib radhiallahu ‘anhu. Kemudian bibi dari Khalid bin al-Walid. Dan juga bibi Abdullah bin Abbas (adz-Dzahabi: Siyar A’lam an-Nubala, 2/238).
+
+Keistimewaan lain dari Ibunda Maimunah adalah ia meriwayatkan tujuh hadits yang termaktub dalam Shahih al-Bukhari dan Muslim. Satu hadits yang diriwayatkan al-Bukhari saja. Lima hadits oleh Muslim saja. Dan total hadits-hadits yang ia riwayatkan sebanyak tiga belas hadits (adz-Dzahabi: Siyar A’lam an-Nubala, 2/238).
+
+Rasulullah shallallahu ‘alaihi wa sallam menyebut Ummul Mukminin Maimunah dan saudari-saudarinya sebagai wanita yang beriman. Dari Abdullah bin Abbas radhiallahu ‘anhuma, Rasulullah shallallahu ‘alaihi wa sallam bersabda,
+
+الأَخَوَاتُ مُؤْمِنَاتٌ: مَيْمُونَةُ زَوْجُ النَّبِيِّ، وَأُمُّ الْفَضْلِ بنتُ الْحَارِثِ، وسَلْمَى امْرَأَةُ حَمْزَةَ، وَأَسْمَاءُ بنتُ عُمَيْسٍ هِيَ أُخْتُهُنَّ لأُمِّهِنَّ
+
+“Saudara-saudara perempuan yang beriman adalah Maimunah istri Nabi, Ummul Fadhl binti al-Harits, Salma istri dari Hamzah, dan Asma binti Umais yang merupakan saudara se-ibu mereka.” (HR. al-Hakim dalam al-Mustadrak 6801. Ia mengatakan shahih sesuai dengan syarat Muslim).
+
+Menikah dengan Rasulullah
+
+Kisah cinta itu bermula saat Maimunah hidup sendiri tanpa suami. Al-Abbas yang merupakan orang dekat Maimunah dan orang dekat Rasulullah, menawarkan Maimunah kepada Rasulullah saat sedang di Juhfah. Rasulullah pun tertarik kemudian menikahinya. Tanah di Sarif, 10 Mil dari Mekah menjadi saksi rumah tangga ini mulai dibina. Pernikahan ini berlangsung pada tahun ke-7 H (629 M) saat umrah qadha. Melalui Abbas, Rasulullah membayarkan mahar senilai 400 Dirham.
+
+Sebelumnya, Maimunah merupakan istri dari Abu Ruhm bin Abdul Uzza bin Abu Qais bin Abdu Wud bin Nashr bin Malik bin Hasl bin Amir bin Luay. Ada yang menyatakan bahwa Maimunah adalah wanita yang menawarkan dirinya kepada Nabi. Saat menunggai ontanya, Maimunah berkata, “Tunggangan ini dan apa yang ada di atasnya adalah untuk Allah dan Rasul-Nya.” Allah Ta’ala menurunkan firma-Nya,
+
+وَامْرَأَةً مُؤْمِنَةً إِنْ وَهَبَتْ نَفْسَهَا لِلنَّبِيِّ إِنْ أَرَادَ النَّبِيُّ أَنْ يَسْتَنْكِحَهَا خَالِصَةً لَكَ مِنْ دُونِ الْمُؤْمِنِينَ
+
+“Dan perempuan mukminah yang menyerahkan dirinya kepada Nabi kalau Nabi mau menikahinya, sebagai pengkhususan bagimu, bukan untuk semua orang mukmin.” [Quran Al-Ahzab: 50]
+
+Ada pula yang mengatakan bahwa nama Maimunah dulu adalah Barrah. Kemudian Rasulullah menggantinya dengan Maimunah. Ia adalah istri yang sangat dekat dengan Nabi. Di antara bukti kedekatannya adalah Nabi terbiasa mandi bersamanya melalui satu wadah air yang sama (Muhibbuddin ath-Thabari: as-Simthu ats-Tsamin, Hal: 192).
+
+Hikmah Pernikahan
+
+Dengan menikahi Maimunah, Nabi Muhammad shallallahu ‘alaihi wa sallam telah melakukan kemaslahatan besar. Hubungan ipar dengan Bani Hilal berhasil menarik simpati mereka. Membuat mereka tak ragu memeluk agama yang mulia ini. Karena mereka tak merasa ada jarak antara mereka dengan Nabi Muhammad. Hilanglah rasa kikuk dan berat di hati. Taka da lagi gengsi, karena mereka telah menjadi satu keluarga (Muhammad Fathi Mus’id: Ummahatul Mukminin, Hal: 206).
+
+Muhammad Rasyid Ridha rahimahullah mengatakan, “Paman Nabi, Abbas, lah yang sangat menginginkan terjadinya pernikahan ini. Ditambah lagi Maimunah merupakan saudari istrinya, Lubabah al-Kubra Ummul Fadhl. Ia mengikatkan keduanya atas izin Ummul Fadhl. Sekiranya Abbas tidak melihat adanya maslahat besar dari pernikahan ini, tentu ia tak akan sebegitu perhatian terhadap hal ini (Muhammad Rasyid Ridha: Nida Li al-Jinsi al-Lathif fi Huquq an-Nisa fi al-Islam, Hal: 84).
+
+Di Rumah Tangga Nabawi
+
+Ibnu Hisyam mengatakan, “Maimunah menyerahkan urusannya kepada saudarinya, Ummul Fadhl. Lalu Ummul Fadhl membicarakannya dengan suaminya, Abbas. Sampai akhirnya Rasulullah menikahinya. Dan memberi mahar sebanyak 400 Dirham (Ibnu Katsir: as-Sirah an-Nabawiyah: 3/439).
+
+Ada pula yang mengatakan Maimunah langsung yang menawarkan dirinya kepada Nabi. Kemudian Allah Ta’ala menurunkan firman-Nya,
+
+وَامْرَأَةً مُؤْمِنَةً إِنْ وَهَبَتْ نَفْسَهَا لِلنَّبِيِّ إِنْ أَرَادَ النَّبِيُّ أَنْ يَسْتَنْكِحَهَا خَالِصَةً لَكَ مِنْ دُونِ الْمُؤْمِنِينَ
+
+“Dan perempuan mukminah yang menyerahkan dirinya kepada Nabi kalau Nabi mau menikahinya, sebagai pengkhususan bagimu, bukan untuk semua orang mukmin.” [Quran Al-Ahzab: 50]
+
+Maimunah berkata, “Onta dan apa yang ada di atasnya milik Rasulullah.” (Ibnu Hisyam: as-Sirah an-Nabawiyah, 2/646).
+
+Dengan menjadi istri Rasulullah shallallahu ‘alaihi wa sallam, Maimunah binti al-Harits radhiallahu ‘anha bergabung dengan istri-istri lainnya. Mereka sama-sama memiliki peranan dalam menyampaikan kehidupan Rasulullah kepada umatnya. Sebagaimana firman Allah Ta’ala,
+
+وَاذْكُرْنَ مَا يُتْلَى فِي بُيُوتِكُنَّ مِنْ آيَاتِ اللَّهِ وَالْحِكْمَةِ إِنَّ اللَّهَ كَانَ لَطِيفًا خَبِيرًا
+
+“Dan ingatlah apa yang dibacakan di rumahmu dari ayat-ayat Allah dan hikmah (sunnah nabimu). Sesungguhnya Allah adalah Maha Lembut lagi Maha Mengetahui.” [Quran Al-Ahzab: 34]
+
+Di antara tugas istri-istri Nabi Muhammad shallallahu ‘alaihi wa sallam adalah menyampaikan hukum-hukum syariat, aktivitas-aktivitas Nabi, yang tidak dilihat oleh masyarakat umum. Mereka menyampaikan bagaimana Nabi mandi dan berwudhu. Apa yang dilakukan Nabi sebelum dan saat tidur, juga saat bangun dari tidur. Saat masuk dan keluar rumah, dan hal-hal lainnya yang tidak bisa diketahui secara detil kecuali oleh istri-istri beliau radhiallahu ‘anhunna. Merekalah yang senantiasa bersama Nabi saat di rumah.
+
+Wafatnya
+
+Ummul Mukmini Maimunah binti al-Harits radhiallahu ‘anha wafat di daerah Sarif. Sebuah tempat antara Mekah dan Madinah. Tempat pertama ia membangun rumah tangga dengan Rasulullah shallallahu ‘alaihi wa sallam.
+
+Beliau wafat pada tahun 51 H/671 M, sebagaimana yang dikuatkan oleh Ibnu Hajar dan selainnya. Saat wafat, usia beliau adalah 80 atau 81 tahun (Ibnu Saad: ath-Thabaqat al-Kubra, 8/140).
+
+
+
+Read more https://kisahmuslim.com/6213-ummul-mukminin-maimunah-binti-al-harits.html'''),
+              SizedBox(
+                height: 42,
+              ),
+              Wrap(
+                alignment: WrapAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Container(
+                      color: Color.fromRGBO(0, 0, 0, 0.04),
+                      padding: EdgeInsets.only(
+                          right: 12, left: 12, top: 8, bottom: 8),
+                      child: Text(
+                        "nahlmb.dev - 2020 - IDN Final Task\nsumber : kisahmuslim.com",
+                        style: TextStyle(color: Colors.black54),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 42,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
